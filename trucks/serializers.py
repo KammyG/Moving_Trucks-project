799d033d27be19d09aef2008a06285cfc490a9f7
kami <muriukichristiana@gmail.com>
2025-04-05 11:48:04 +0300

    Fix BookingSerializer to reference customer field and update Booking creation logic

--- trucks/serializers.py
+++ trucks/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Truck, Booking, Review
 from django.contrib.auth.models import User
+from rest_framework import permissions
+from rest_framework import generics
 
 User = get_user_model()
 
@@ -30,8 +32,15 @@ class BookingSerializer(serializers.ModelSerializer):
         fields = ['id', 'truck', 'pickup_location', 'dropoff_location', 'date', 'price', 'customer']  
         read_only_fields = ['customer', 'price']
 
+class BookingListView(generics.ListCreateAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]  
 
-# Review Serializer
+    def perform_create(self, serializer): 
+
+        serializer.save(customer=self.request.user)
+
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
